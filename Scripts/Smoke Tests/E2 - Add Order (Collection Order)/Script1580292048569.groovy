c8de8a667eb6db021e8d9669998c9ee2dfa3f13b
Ravi Balasundaram <rbalasundaram@savanainc.com>
2020-02-11 16:32:31 +0530

    Completed Payment and Collection order Test Cases

--- Scripts/Smoke Tests/E2 - Add Order (Collection Order)/Script1580292048569.groovy
+++ Scripts/Smoke Tests/E2 - Add Order (Collection Order)/Script1580292048569.groovy
@@ -14,4 +14,178 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import utils.MapUtil as MapUtil
+import constants.Urls as Urls
+import constants.ColumnPosition as ColumnPosition
+import constants.Fields as Fields
+import constants.Operator as Operator
+import data.ConsumerTempData as ConsumerData
+int LATEST_ROW = 1
+
+int WAIT_FOR_FIVE_SECONDS = 5
+int SUBSTRING_DATE_START = 0
+int SUBSTRING_DATE_END = 10
+String PAYMENT_ORDER_CASE_TYPE = "Inbound ACH Collection Order"
+String ORDER_STATUS = "Entered"
+Map<Fields, String> customerData = ConsumerData.CUSTOMERDATA_MAP
+Map<Fields, String> custOrderData = ConsumerData.ACCOUNT_COLLECTION_ORDER
+
+TestObject scheduledTransactionsTable = findTestObject('Account/AccountTaskDrawer/AddOrder/table_Orders')
+TestObject openCases = findTestObject('Account/AccountDashboardPage/CasesSection/table_OpenCases')
+
+'Login into portal'
+CustomKeywords.'pages.LoginPage.loginIntoPortal'()
+
+'Navigate To customer dashboard'
+WebUI.navigateToUrl(custOrderData.get(Fields.URL))
+
+
+'Click on Account Task Drawer'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/task_Drawer'))
+
+'Click on Add Order Tasks'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/task_AddOrder'))
+
+'Click on Order Type DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/select_OrderType'))
+
+
+'Select Order type from OrderType DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/options_InSelect',[('value'):custOrderData.get(Fields.ORDER_TYPE)]))
+
+
+'Click on Origin Source DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/select_OriginSource'))
+
+
+'Select Origin Source from OriginSource DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/options_InSelect',[('value'):custOrderData.get(Fields.ORDER_ORIGIN_SOURCE)]))
+
+
+'Click on order Source DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/select_OrderSource'))
+
+
+'Select order Source from OriginSource DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/options_InSelect' ,[('value'):custOrderData.get(Fields.ORDER_SOURCE)]))
+
+'Click on Operational area  DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/select_OperationalArea'))
+
+
+'Select Operational area from OperationalArea DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/options_InSelect' ,[('value'):custOrderData.get(Fields.ORDER_OPERATIONL_AREA)]))
+
+'Type Order Info'
+CustomKeywords.'actions.WebActions.typeText'(findTestObject('Account/AccountTaskDrawer/AddOrder/input_OrderInfo'), custOrderData.get(Fields.ORDER_INFO))
+
+
+'Type Routing Number'
+CustomKeywords.'actions.WebActions.typeText'(findTestObject('Account/AccountTaskDrawer/AddOrder/input_RoutingNumber'), custOrderData.get(Fields.ORDER_ROUTING_NUMBER))
+
+
+'Type AccountTitle'
+CustomKeywords.'actions.WebActions.typeText'(findTestObject('Account/AccountTaskDrawer/AddOrder/input_AccountTitle'), custOrderData.get(Fields.ORDER_COUNTERPARTY_ACCOUNT_TITLE))
+
+
+'Click on Account Group DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/select_AccountGroup'))
+
+
+'Select Account Group from Account Group DropDown'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/options_InSelect',[('value'):custOrderData.get(Fields.ORDER_COUNTERPARTY_ACCOUNT_GROUP)]))
+
+'Type Account Number'
+CustomKeywords.'actions.WebActions.typeText'(findTestObject('Account/AccountTaskDrawer/AddOrder/input_AccountNum'), custOrderData.get(Fields.ORDER_COUNTERPARTY_FROM_ACCOUNT_NUMBER))
+
+'Click on Send now Checkbox'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/checkbox_SendNow'))
+
+'Type transfer amount'
+CustomKeywords.'actions.WebActions.typeText'(findTestObject('Account/AccountTaskDrawer/AddOrder/input_OrderAmt'), custOrderData.get(Fields.ORDER_TRANSFER_AMOUNT))
+
+
+'Click on Submit Button'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/btn_Submit'))
+
+'Click on Confirm Button'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountTaskDrawer/AddOrder/btn_Confirm'))
+
+
+
+
+'Verify Money Movement Case Alert Displayed'
+WebUI.verifyElementVisible(findTestObject('Account/AccountTaskDrawer/AddOrder/alert_MoneyMovement'))
+
+WebUI.delay(WAIT_FOR_FIVE_SECONDS)
+
+'Scroll to Schedule Transactions Table'
+WebUI.scrollToElement(scheduledTransactionsTable, GlobalVariable.Timeout)
+
+
+
+'Verify Transactions(Scheduled) Table with the updated Order Book Transfer(Order Type)'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.ORDER_TYPE,
+	custOrderData.get(Fields.ORDER_TYPE), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify Order Created  Date in Schedule Transactions Table'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.ORDER_CREATED_DATE_IN_TRANSACTIONS,
+	custOrderData.get(Fields.ORDER_CREATED_DATE).substring(SUBSTRING_DATE_START, SUBSTRING_DATE_END), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify  Order Date in Schedule Transactions Table'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.ORDER_DATE,
+	custOrderData.get(Fields.ORDER_DATE).substring(SUBSTRING_DATE_START, SUBSTRING_DATE_END), Operator.EQUALS_IGNORE_CASE)
+
+
+
+'Verify Transactions(Scheduled) Table with the updated Order Book Transfer(From Account)'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.FROM_ACCOUNT,
+	custOrderData.get(Fields.ORDER_COUNTERPARTY_FROM_ACCOUNT_NUMBER), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify Transactions(Scheduled) Table with the updated Order Book Transfer(To Account)'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.TO_ACCOUNT,
+	custOrderData.get(Fields.ORDER_COUNTERPARTY_TO_ACCOUNT_NUMBER), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify Transactions(Scheduled) Table with the updated Order Book Transfer(Order Amount)'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.ORDER_AMOUNT,
+	custOrderData.get(Fields.ORDER_TRANSFER_AMOUNT), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify Transactions(Scheduled) Table with the updated Order Book Transfer(Order Status)'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(scheduledTransactionsTable, LATEST_ROW, ColumnPosition.ORDER_STATUS,
+	ORDER_STATUS, Operator.EQUALS_IGNORE_CASE)
+
+
+'Click on Cases Tab'
+CustomKeywords.'actions.WebActions.click'(findTestObject('Account/AccountDashboardPage/TabSection/tab_Cases'))
+
+
+'Verify Case Type in Open cases Tab'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(openCases, LATEST_ROW, ColumnPosition.CASE_TYPE,
+	PAYMENT_ORDER_CASE_TYPE, Operator.EQUALS_IGNORE_CASE)
+
+'Verify Account Number in Open cases Tab'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(openCases, LATEST_ROW, ColumnPosition.ACCOUNT_NUMBER,
+	custOrderData.get(Fields.ORDER_COUNTERPARTY_TO_ACCOUNT_NUMBER), Operator.EQUALS_IGNORE_CASE)
+
+
+'Verify Order Created Date in Open cases Tab'
+CustomKeywords.'actions.WebTable.verifyCellValueMatches'(openCases, LATEST_ROW, ColumnPosition.ORDER_CREATED_DATE,
+	custOrderData.get(Fields.ORDER_CREATED_DATE).substring(SUBSTRING_DATE_START, SUBSTRING_DATE_END), Operator.EQUALS_IGNORE_CASE)
+
+
+
+
+
+
+
+
+
+
+
+
 
