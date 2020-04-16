b19eda8c3134987395b95d2baa31934b90523cb9
Ravi Balasundaram <rbalasundaram@savanainc.com>
2020-04-16 06:01:12 -0400

    Added CE Smoke Suite

---
+++ Test Suites/CE-Smoke.groovy
@@ -0,0 +1,62 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.annotation.SetUp as SetUp
+import com.kms.katalon.core.annotation.SetupTestCase as SetupTestCase
+import com.kms.katalon.core.annotation.TearDown as TearDown
+import com.kms.katalon.core.annotation.TearDownTestCase as TearDownTestCase
+
+// Please change skipped to be false to activate this method.
+// Put your code here.
+/**
+ * Clean test suites environment.
+ */
+// Please change skipped to be false to activate this method.
+// Put your code here.
+/**
+ * Run before each test case starts.
+ */
+// Please change skipped to be false to activate this method.
+// Put your code here.
+/**
+ * Run after each test case ends.
+ */
+// Please change skipped to be false to activate this method.
+// Put your code here.
+
+@SetUp(skipped = false)
+def setUp() {
+	
+	WebUI.callTestCase(findTestCase('Test Cases/Base API Calls/A0 - Create Consumer'), null)
+	WebUI.callTestCase(findTestCase('Test Cases/Base API Calls/A1 - Create Personal Savings Account'), null)
+    WebUI.callTestCase(findTestCase('Test Cases/Base API Calls/A2 - Create Multi-Position account'), null)
+	WebUI.callTestCase(findTestCase('Test Cases/Base API Calls/A3 - Create Organization'), null)
+    WebUI.callTestCase(findTestCase('Test Cases/Base API Calls/A4 - Credit Money to Savings account'), null)
+}
+
+@TearDown(skipped = false)
+def tearDown() {
+	WebUI.closeBrowser()
+}
+
+@SetupTestCase(skipped = true)
+def setupTestCase() {
+}
+
+@TearDownTestCase(skipped = true)
+def tearDownTestCase() {
+}
---
+++ Test Suites/CE-Smoke.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CE-Smoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>dc3ed0b8-5d52-45a1-8ff0-89b1360d0038</testSuiteGuid>
+   <testCaseLink>
+      <guid>ecbbe25f-4297-40d3-a8ff-3fb2c7d2c978</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A0 - Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0e986a0f-748e-400e-b894-ffc1dd9a08aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A1 - Check Version</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a029cc8-00f5-4437-8660-cd1decd37497</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A2 - Reset Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b15d290e-a5f4-4b1c-b7a7-c7660e0fee0d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A3 - Update Temporary password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bc85b148-0a45-47c2-a3dd-8e60a5d9ab39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A4 - Search Customer (Does not exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>561dfcc5-9e48-41f1-86ca-b6222e6c0535</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A5 - Search Customer (exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a9e291a-c359-40f4-a85a-39ed3be36c76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A6 - Search Organization (Does not exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a760ed57-3223-4dfe-9726-b1caa40bb1b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A7 - Search Organizarion (exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8536cadb-008f-4e20-93e9-fdad2cec1c34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A8 - Search Account (Does not exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87d1596d-6c2a-4287-a40e-e936f968f7e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/A9 - Search Account (exist)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>80073d47-57f0-4388-bfb4-d118cc36ed2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/B0 - Create Customer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38d96bb8-6697-4db5-ba90-ff82f5bf2eda</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/B1 - Create Multi-Position Account for Customer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c12932bd-fd87-452f-ac59-3caa1a8e0917</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/B2 - Create Banking Account for Customer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04db75ed-5bf8-4b1e-86e1-c158534d87ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/C0 - Add Related party to a Banking Account</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2f2f4c3-249d-40f1-a832-1163eab8f73c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/C1 - Post a transaction for Banking Account (Credit)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>175ec479-3cfc-4cc8-9715-54d2393eb7af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/C2 - Post a transaction for Banking Account (Debit)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9e711c3-2d03-4c7b-be1a-e0a2c5a88cca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/C3 - Post a transaction for Multi-position Account (Credit)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>244c31ad-68e2-4a09-8d3f-4ca5a1ea9363</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/C4 - Post a transaction for Multi-position Account (Debit)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f1c01057-195a-40f8-9ee5-f898fe5d6b16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/E0 - Add Order (Book Transfer)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fd7081e0-4b66-40e6-8ceb-ab5c5686d28b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/E1 - Add Order (Payment Order)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dec8e81a-f037-43ea-af7e-17c4e4fbaf18</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/E2 - Add Order (Collection Order)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5df72213-21e7-4e0a-ae99-3d7bb077509f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CE-Smoke Test/I0 - Create Organization</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
